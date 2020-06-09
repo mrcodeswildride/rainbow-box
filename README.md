328f36c09d4313028684b13ab4ab4a11f6a81b5a
David Keller <mrcodeswildride@gmail.com>
2020-06-09 12:18:59 +0000

    es6 update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # rainbow-box
 
-This is a rainbow box web app written in HTML, CSS, and JavaScript.
+Rainbow box web app written in HTML, CSS, and JavaScript.
 
 Use this app at [rainbowbox.mrcodeswildride.com](https://rainbowbox.mrcodeswildride.com/).
 Learn how to make this app at [mrcodeswildride.com](https://www.mrcodeswildride.com/).
--- script.js
+++ script.js
@@ -1,43 +1,44 @@
-var messageParagraph = document.getElementById("message");
-var box = document.getElementById("box");
+let messageParagraph = document.getElementById(`messageParagraph`)
+let box = document.getElementById(`box`)
 
-var gameStarted = false;
-var points = 0;
-var intervalId = null;
+let gameStarted = false
+let score = 0
+let intervalId
 
-box.addEventListener("click", clickBox);
+box.addEventListener(`click`, clickBox)
 
 function clickBox() {
-    if (gameStarted == false) {
-        gameStarted = true;
-        messageParagraph.innerHTML = "Click the box when it's blue.";
-        box.style.backgroundColor = "";
-        points = 0;
-        intervalId = setInterval(changeColor, 500);
+  if (gameStarted == false) {
+    messageParagraph.innerHTML = `Click the box when it's blue.`
+    intervalId = setInterval(changeColor, 500)
+    gameStarted = true
+
+    box.style.backgroundColor = ``
+    score = 0
+  }
+  else {
+    if (box.style.backgroundColor == `blue`) {
+      score = score + 1
+      messageParagraph.innerHTML = `Score: ${score}`
     }
     else {
-        if (box.style.backgroundColor == "blue") {
-            points = points + 1;
-            messageParagraph.innerHTML = "Points: " + points;
-        }
-        else {
-            gameStarted = false;
-            messageParagraph.innerHTML = "You misclicked. Your score is " + points + (points == 1 ? " point." : " points.");
-            clearInterval(intervalId);
-        }
+      messageParagraph.innerHTML = `You misclicked. Your score is ${score}.`
+      clearInterval(intervalId)
+      gameStarted = false
     }
+  }
 }
 
 function changeColor() {
-    var randomNumber = Math.floor(Math.random() * 3);
+  let randomNumber = Math.floor(Math.random() * 3)
 
-    if (randomNumber == 0) {
-        box.style.backgroundColor = "red";
-    }
-    else if (randomNumber == 1) {
-        box.style.backgroundColor = "green";
-    }
-    else if (randomNumber == 2) {
-        box.style.backgroundColor = "blue";
-    }
+  if (randomNumber == 0) {
+    box.style.backgroundColor = `red`
+  }
+  else if (randomNumber == 1) {
+    box.style.backgroundColor = `green`
+  }
+  else if (randomNumber == 2) {
+    box.style.backgroundColor = `blue`
+  }
 }
